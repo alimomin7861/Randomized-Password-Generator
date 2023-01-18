6568480b067e708a012286dc98087b83ecbfc97f
Ali Momin <Ali@Alis-MBP-2.attlocal.net>
2023-01-18 18:33:40 -0500

    added for loop to generate random passowrd

--- script.js
+++ script.js
@@ -14,19 +14,25 @@ var addLower;
 var addNumber; 
 var addSpecialChar;
 
+
 // Create generatePassword function 
-var generatePassword() {
+function generatePassword() {
+
     // Stores password length value in addLength
     var addLength = prompt("Enter your desired password length (8 - 128 characters).")
 
-    // Conditional statement if user inputs character length < 8 OR > 128.
-    if (addLength < 8){
+    // Conditional statement if user inputs invalid character length
+    if (addLength <= 7 || addLength >= 129){
         alert ("Your desired password length must be between 8 - 128 characters.")
-    } else if (addLength > 128) {
-        alert ("Your desired password length must be between 8 - 128 characters.")
-    } else {
+
         var addLength = prompt("Enter your desired password length (8 - 128 characters).")
+    } else {
+        alert ("Your desired password length must be between 8 - 128 characters.")
+        return generatePassword();
     }
+
+    // Password length is accepted (between 8 - 128 chracters) message to user
+    alert("Your desired password length is " + addLength + " characters.")
 
     // Add values to user-inputted variables 
     addUpper = confirm("Click OK to confirm including uppercase characters."); 
@@ -34,7 +40,7 @@ var generatePassword() {
     addNumber = confirm("Click OK to confirm including numeric characters.");
     addSpecialChar = confirm("Click OK to confirm including special characters.")
 
-    // WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected
+    // Conditional state if the user does not select any password criterias to include in password
     if (addUpper == false && addLower == false && addNumber == false && addSpecialChar == false) {
         alert("At least one criteria type is needed to generate randomized password.")
 
@@ -49,37 +55,35 @@ var generatePassword() {
 
         // Uppercase characters will be included in the randomized password
     if (addUpper == true){
-        passwordCriteria = passwordCriteria.concat(addUpper)
+        passwordCriteria = passwordCriteria.concat(upper)
     }
         // Lowercase characters will be included in the randomized password
     if (addLower == true){
-        passwordCriteria = passwordCriteria.concat(addLower)
+        passwordCriteria = passwordCriteria.concat(lower)
     }
         // Numeric characters will be included in the randomized password
     if (addNumber == true){
-        passwordCriteria = passwordCriteria.concat(addNumber)
+        passwordCriteria = passwordCriteria.concat(number)
     }
         // Special characters will be included in the randomized password
     if (addSpecialChar == true){
-        passwordCriteria = passwordCriteria.concat(addSpecialChar)
+        passwordCriteria = passwordCriteria.concat(specialChar)
     }
 
     // Generate random password 
-    var randomPassword = "";
 
-    const randomPassword = Math.floor(Math.random() * randomPassword.length);
+    var randomPassword = [];
 
+    for ( let i = 0; i < addLength; i++){
+        var results = passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
+        randomPassword.push(results);
+    }
 
-
-
-
-
-
-
+    return randomPassword.join("");
 }
 
 
-// Write password to the #password input
+    // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
