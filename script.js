1f5571aa6f4e02336327e6a95ed758fe745c0dfc
Ali Momin <Ali@Alis-MBP-2.attlocal.net>
2023-01-17 22:24:54 -0500

    password length conditional done

--- script.js
+++ script.js
@@ -7,11 +7,32 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "#", "$", "&", "(", ")", "+", ",", "=", ">", "<", "?", "@", "*"];
 
-var length = "";
+// Define User-inputted variables
+var addLength = "";
 var addUpper;
 var addLower; 
 var addNumber; 
 var addSpecialChar;
+
+// Create generatePassword function 
+var generatePassword() {
+    // Stores password length value in addLength
+    var addLength = prompt("Enter your desired password length (8 - 128 characters).")
+
+    // Conditional statement if user inputs character length < 8 OR > 128.
+    if (addLength < 8){
+        alert ("Your desired password length must be between 8 - 128 characters.")
+    } else if (addLength > 128) {
+        alert ("Your desired password length must be between 8 - 128 characters.")
+    } else {
+        var addLength = prompt("Enter your desired password length (8 - 128 characters).")
+    }
+
+
+
+
+
+}
 
 
 // Write password to the #password input
